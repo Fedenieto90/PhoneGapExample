5f78642730f47e72e8e4c6d5948fdd8da30fc370
Fede Nieto <fedenieto90@gmail.com>
2014-04-28 01:14:36 -0300

    Prueba

--- script.js
+++ script.js
@@ -1,6 +1,100 @@
 angular.module('ionicApp', ['ionic'])
 
-.controller('MyCtrl', function ($scope) {
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+  .state('menu', {
+      url: "/menu",
+      templateUrl: "menu.html",
+      controller: 'SignInCtrl'
+    })
+  .state('actions', {
+      url: "/main-actions",
+      templateUrl: "main-actions.html",
+      controller: 'SignInCtrl'
+    })
+    .state('signin', {
+      url: "/sign-in",
+      templateUrl: "sign-in.html",
+      controller: 'SignInCtrl'
+    })
+    .state('forgotpassword', {
+      url: "/forgot-password",
+      templateUrl: "forgot-password.html"
+    })
+    .state('tabs', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "tabs.html"
+    })
+    .state('tabs.home', {
+      url: "/home",
+      views: {
+        'home-tab': {
+          templateUrl: "home.html",
+          controller: 'HomeTabCtrl'
+        }
+      }
+    })
+    .state('tabs.facts', {
+      url: "/facts",
+      views: {
+        'home-tab': {
+          templateUrl: "facts.html"
+        }
+      }
+    })
+    .state('tabs.facts2', {
+      url: "/facts2",
+      views: {
+        'home-tab': {
+          templateUrl: "facts2.html"
+        }
+      }
+    })
+    .state('tabs.about', {
+      url: "/about",
+      views: {
+        'about-tab': {
+          templateUrl: "about.html"
+        }
+      }
+    })
+    .state('tabs.navstack', {
+      url: "/navstack",
+      views: {
+        'about-tab': {
+          templateUrl: "nav-stack.html"
+        }
+      }
+    })
+    .state('tabs.contact', {
+      url: "/contact",
+      views: {
+        'contact-tab': {
+          templateUrl: "contact.html"
+        }
+      }
+    });
+    
+
+
+
+   $urlRouterProvider.otherwise("/sign-in");
+
+})
+
+.controller('SignInCtrl', function($scope, $state) {
+
+  $scope.signIn = function(user) {
+    console.log('Sign-In', user);
+    $state.go('actions');
+  };
+
+   $scope.scan = function() {
+    console.log('Scanned Url');
+    $state.go('menu');
+  };
 
   $scope.data = {
     showDelete: false
@@ -60,4 +154,8 @@ angular.module('ionicApp', ['ionic'])
     }
   ];
 
+})
+
+.controller('HomeTabCtrl', function($scope) {
+  console.log('HomeTabCtrl');
 });
